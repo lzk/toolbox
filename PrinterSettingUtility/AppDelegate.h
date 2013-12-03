12d1e432107cad5d868341d368e5d8ae3099a8cf
Wang Kun <WangKun@WangdeiMac.local>
2013-12-03 13:21:41 +0800

    Initial Commit

---
+++ PrinterSettingUtility/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PrinterSettingUtility
+//
+//  Created by Wang Kun on 13-12-3.
+//  Copyright (c) 2013年 Wang Kun. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PrinterSettingUtility/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  PrinterSettingUtility
+//
+//  Created by Wang Kun on 13-12-3.
+//  Copyright (c) 2013年 Wang Kun. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
